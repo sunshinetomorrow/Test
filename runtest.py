5867fbdb057bcfdd1a4894e32f82c31d3f87b5e3
“sunshinetomorrow” <jiangjg24@126.com>
2017-10-08 20:21:25 +0800

    init

---
+++ runtest.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import time, sys
+from HTMLTestRunner import HTMLTestRunner
+import unittest
+# sys.path.append('./test_cases')
+
+'''参考资料：http://www.cnblogs.com/yufeihlf/p/5752146.html'''
+
+
+test_dir = './test_cases'
+discover = unittest.defaultTestLoader.discover(test_dir, pattern='test_*.py')
+
+
+
+if __name__ == "__main__":
+
+    now = time.strftime("%Y-%m-%d %H_%M_%S")
+    filename = './report/' + now + '_result.html'
+    fp = open(filename, 'wb')
+    runner = HTMLTestRunner(stream=fp,
+                            title='Test Report',
+                            description='Implementation  with: ')
+    runner.run(discover)
+    fp.close()
